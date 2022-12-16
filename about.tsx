7ad4d2255eb12c5b5fd4f8151127eb538883d999
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-12-16 19:02:55 +0100

    mock tsx file

---
+++ about.tsx
@@ -0,0 +1,9 @@
+export const About = () => (
+  <>
+    <h1>I added this file, just so GitHub shows TS as used languages :p</h1>
+    <span>
+      I wanted to make it a little bit more obvius the repo is targeting TS and
+      TSX
+    </span>
+  </>
+);
